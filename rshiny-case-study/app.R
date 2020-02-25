fe81d0f31f8002b86749787426906f11d3a65528
bernardo <bernardo.dauria@uc3m.es>
2020-02-25 12:37:34 +0100

    empty app

---
+++ rshiny-case-study/app.R
@@ -0,0 +1,10 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(title="Shiny App")
+
+# Define server logic required to draw a histogram
+server <- function(input, output) { }
+
+# Run the application 
+shinyApp(ui = ui, server = server)
