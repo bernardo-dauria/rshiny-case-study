e3055475ec59ce28f0b1ea2f6dc6c535e1b59611
bernardo <bernardo.dauria@uc3m.es>
2020-02-25 18:41:27 +0100

    adding a panel showing a plot

--- rshiny-case-study/app.R
+++ rshiny-case-study/app.R
@@ -7,19 +7,31 @@ dataPanel <- tabPanel("Data",
                         inputId = "selYear",
                         label = "Select the Year",
                         multiple = TRUE,
-                        choices = gapminder %>% select(year) %>% unique %>% arrange
+                        choices = gapminder %>% select(year) %>% unique %>% arrange,
+                        selected = 0
                         ),
                       tableOutput("data")
 )
 
+plotPanel <- tabPanel("Plot",
+                      plotOutput("plot")
+                      )
+
 # Define UI for application that draws a histogram
 ui <- navbarPage("shiny App",
-                 dataPanel
+                 dataPanel,
+                 plotPanel
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) { 
-  output$data <- renderTable(gapminder %>% filter(year %in% input$selYear))
+  output$data <- renderTable(gapminder %>% filter(year %in% input$selYear));
+  output$plot <- renderPlot(
+    barplot(head(gapminder %>% filter(year %in% input$selYear) %>% pull(pop)),
+            main=paste("Population in",input$selYear), horiz=FALSE,
+            names.arg= head(gapminder %>% filter(year %in% input$selYear) %>% pull(country))
+    )
+  )
 }
 
 # Run the application 
